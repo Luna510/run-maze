e43917392a1d7202f7dab6544ae632ed5e990eee
Luna510 <145373405+Luna510@users.noreply.github.com>
2024-02-05 09:10:37 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,60 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let opening =document.querySelector(".story-opening");
+let button1 = document.querySelector(".option-one");
+let button2 = document.querySelector("option-two");
+let option1 = document.querySelector(".option-one-screen");
+let option2 = document.querySelector(".option-two-screen");
+let option1end = document.querySelector(".option-oneA-end");
+let option2end = document.querySelector(".option-oneB-end");
+let optionend1 = document.querySelector(".option-one-end");
+let optionend2 = document.querySelector(".option-two-end");
+let buttonRunleft1 = document.querySelector(".Runleft-one");
+let buttonRunrightt1 = document.querySelector(".Runright-one"); 
+
+  button1.onclick = function() {
+      // opening.style.display = "none"; 
+      // buttons.style.display = "none"; 
+      option1.style.display = "block"; };
+     
+  // button2.onclick = function() {
+    // opening.style.display = "none";
+    // buttons.style.display = "none";
+    option1.style.display = "block";
+
+  button1.onclick = function() {
+   // opening.style.display = "none";
+   // buttons.style.display = "none";
+    option1.style.display = "block";
+};
+
+  //button2.onclick = function() {
+    //opening.style.display = "none";
+    //option2.style.display = "block";
+console.log("hi");
+
+ //buttonRunleft1.onclick=function(){
+    option1.style.display="none";
+  //  option1end.style.display="block";
+   console.log("hi");
+
+ //buttonRunright2.onclick=function(){
+    option1.style.display="none";
+    //option2end.style.display="block";
+   console.log("hi");
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
